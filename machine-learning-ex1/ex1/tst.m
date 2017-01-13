1a3d335044fa4cc9615b9a9fe3fc5ece13d04c5b
Sotiris Matzanas <smatzanas@mbp-6018.local>
2017-01-13 10:23:43 -0800

    First

---
+++ machine-learning-ex1/ex1/tst.m
@@ -0,0 +1,19 @@
+%% =================== Part 3: Gradient descent ===================
+fprintf('Running Gradient Descent ...\n')
+
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+
+% Some gradient descent settings
+iterations = 1500;
+alpha = 0.01;
+
+% compute and display initial cost
+computeCost(X, y, theta)
+
+% run gradient descent
+theta = gradientDescent(X, y, theta, alpha, iterations);
+
+% print theta to screen
+fprintf('Theta found by gradient descent: ');
+fprintf('%f %f \n', theta(1), theta(2));
