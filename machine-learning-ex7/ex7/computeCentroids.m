d2ef73d92aca495c754d42dca692a03733918fd2
smatzana <sotiris.matzanas@gmail.com>
2017-03-28 14:26:39 -0700

    Passed

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -25,7 +25,14 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
+for k = 1:K
 
+  % Find which X's are assigned to k
+  kAssigned = X(idx == k, :);
+  Ck = size(kAssigned, 1);
+  centroids(k, :) = sum(kAssigned) ./ Ck;
+  
+end
 
 
 
